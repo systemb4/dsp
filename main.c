bddd262893e3a22b1b47864cac87c0d1f6bc640d
systemb4 <lukasnitzsche@yahoo.com>
2021-11-09 21:50:54 -0600

    linked list works with chars in file

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 struct node {
     char value;
     struct node* next;
 };
-typedef struct node node_t;
 
-node_t *cNode(char value) {
-    node_t *result = malloc(sizeof(node_t));
-    result->value = value;
-    result->next = NULL;
-    return result;
-}
+typedef struct node node_c;
 
-node_t *insertEnd(node_t **head, char value) {
-    node_t *result = malloc(sizeof(node_t));
+node_c *head = NULL;
+
+node_c *insertEnd(node_c **head, char value) {
+    node_c *result = malloc(sizeof(node_c));
     result->value = value;
     result->next = NULL;
 
     if(*head == NULL) {
         *head = result;
     } else {
-        node_t *lastNode = *head;
+        node_c *lastNode = *head;
 
         while(lastNode->next != NULL) {
             lastNode = lastNode->next;
@@ -32,38 +27,29 @@ node_t *insertEnd(node_t **head, char value) {
     }
 }
 
-void printList(node_t *head) {
-    node_t *tmp = head;
+void printList(node_c *head) {
+    node_c *tmp = head;
 
     while(tmp != NULL) {
-        printf("%c ", tmp->value);
+        printf("%c", tmp->value);
         tmp = tmp->next;
     }
     printf("\n");
 }
 
-/*
 void transfer(char name[]) {
-    // check if file in dir and if not printf
     FILE *fptr;
     fptr = fopen(name, "r");
     char c = fgetc(fptr);
     while(c != EOF) {
-        strncat(data, &c, 1);
+        insertEnd(&head, c);
         c = fgetc(fptr);
     }
-
     fclose(fptr);
-}*/
+}
 
 int main(int argc, char *argv[]) {
-    node_t *head = NULL;
-    node_t *tmp;
-
-    insertEnd(&head, 'A');
-    insertEnd(&head, 'B');
-    insertEnd(&head, 'C');
-
+    transfer(argv[1]);
     printList(head);
 
     return 0;
