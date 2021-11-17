b4eb73de58e89a1c9889716a735075427e202a7a
systemb4 <lukasnitzsche@yahoo.com>
2021-11-17 14:43:07 -0600

    edit README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Linear Data Sorting Library
 currently in development
+
+Should this work with something like json, yaml, etc. or a custom format?
+
+## todo
+* search should take in word; check for value with spaces or : or , on either side
+    if value is word return what it is : to or the opposite
+* Number of elements
+* number of definitions
+* documentation of each function
+* documentation of how it works
+* change to a lib
--- main.c
+++ main.c
@@ -53,19 +53,9 @@ static void transfer(char name[]) {
     fclose(fileO);
 }
 
-static char searchFor(node_c *head, char searchValue) {
+static void searchFor(node_c *head, char searchValue) {
     node_c *tmp = head;
     char data[30];
-
-    /*
-     * search should take in word
-     * check for value with spaces or : or , on either side
-     * if value is word return what it is : to or the opposite
-     */
-
-    /*
-     * Fix this to only give one word
-     */
 
     while(tmp != NULL) {
         if(tmp->value == searchValue) {
@@ -79,7 +69,7 @@ static char searchFor(node_c *head, char searchValue) {
         tmp = tmp->next;
     }
 
-    return data;
+    printf("%s\n", data);
 }
 
 int main(int argc, char *argv[]) {
@@ -91,7 +81,7 @@ int main(int argc, char *argv[]) {
     }
 
     printList(head);
-    printf("%s\n", searchFor(head, ','));
+    searchFor(head, ',');
 
     return 0;
 }
