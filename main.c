5d09fba142090da8d3a45ba2ccd334886e57952f
systemb4 <lukasnitzsche@yahoo.com>
2022-01-04 21:16:01 -0600

    idk why is still seg faulting, im not even calling anything from the lib

--- main.c
+++ main.c
@@ -5,18 +5,37 @@
 #include "src/dsp.h"
 
 int main(int argc, char *argv[]) {
-    Node *head = NULL;
+    //Node *head = NULL;
 
+    /*
     if(argc <= 1) {
         fprintf(stderr, "You must give an input file!\n");
         return 0;
     } else {
         transfer(head, argv[1]);
     }
+    */
 
-    printList(head);
-    printf("%c", head->value);
+    //printList(head);
     //searchFor(head, ',');
+
+    Node *test1;
+    Node *test2;
+    Node *test3;
+
+    test1->value = 'a';
+    test1->next = test2;
+
+    test2->value = 'b';
+    test2->next = test3;
+
+    test3->value = 'c';
+    test3->next = NULL;
+
+    //printList(test1);
+    printf("%c\n", test1->value);
+    printf("%c\n", test2->value);
+    printf("%c\n", test3->value);
 
     return 0;
 }
